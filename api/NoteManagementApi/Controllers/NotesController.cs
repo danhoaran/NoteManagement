e3d183ab1a0941191ea2b832d2dcb3422e65b562
danhoaran <43774140+danhoaran@users.noreply.github.com>
2022-09-08 23:19:26 +0100

    Add logging

--- api/NoteManagementApi/Controllers/NotesController.cs
+++ api/NoteManagementApi/Controllers/NotesController.cs
@@ -12,21 +12,25 @@ namespace NoteManagementApi.Controllers
     public class NotesController : ControllerBase
     {
         private readonly INoteService _noteService;
+        private readonly ILogger _logger;
 
-        public NotesController(INoteService noteService)
+        public NotesController(INoteService noteService, ILogger<NotesController> logger)
         {
             _noteService = noteService;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<NoteForListingDto>>> GetAllNotes()
         {
+            _logger.LogInformation("GetAllNotes accessed at: {time}", DateTimeOffset.UtcNow);
             return Ok(await _noteService.GetAllNotesAsync());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<NoteDto>> GetNote(int id)
         {
+            _logger.LogInformation("GetNote accessed at: {time}", DateTimeOffset.UtcNow);
             try
             {
                 return Ok(await _noteService.GetNoteByIdAsync(id));
@@ -40,6 +44,7 @@ namespace NoteManagementApi.Controllers
         [HttpGet("{id}/html")]
         public async Task<ActionResult<NoteForHtmlDto>> GetNoteWithHtml(int id)
         {
+            _logger.LogInformation("GetNoteWithHtml accessed at: {time}", DateTimeOffset.UtcNow);
             try
             {
                 return Ok(await _noteService.GetNoteForHtmlAsync(id));
@@ -53,6 +58,7 @@ namespace NoteManagementApi.Controllers
         [HttpPost]
         public async Task<ActionResult<NoteCreationResponseDto>> Create(NoteForCreationDto noteForCreationDto)
         {
+            _logger.LogInformation("Create note accessed at: {time}", DateTimeOffset.UtcNow);
             await _noteService.CreateNoteAsync(noteForCreationDto);
             return Ok(new NoteCreationResponseDto { Success = true });
         }
@@ -60,6 +66,7 @@ namespace NoteManagementApi.Controllers
         [HttpPut]
         public async Task<ActionResult> Update(NoteForUpdateDto dto)
         {
+            _logger.LogInformation("Update note api accessed at: {time}", DateTimeOffset.UtcNow);
             await _noteService.UpdateNoteAsync(dto);
             return NoContent();
         }
@@ -68,6 +75,7 @@ namespace NoteManagementApi.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult> Delete(int id)
         {
+            _logger.LogInformation("Delete note api accessed at: {time}", DateTimeOffset.UtcNow);
             await _noteService.DeleteNoteAsync(id);
             return NoContent();
         }
--- api/NoteManagementApi/Program.cs
+++ api/NoteManagementApi/Program.cs
@@ -1,10 +1,31 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using NoteManagementCore.Services;
 using NoteManagementInfrastructure;
 using NoteManagementServices.Services;
+using Serilog;
+using static System.Net.Mime.MediaTypeNames;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.File("logs/log.txt")
+    .CreateLogger();
+
+builder.Logging.AddSerilog();
+
+//ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+//{
+//builder.AddConsole();
+//builder.AddDebug();
+//builder.AddFile($"\\Logs\\myapp.txt");
+//});
+
+//builder.Logging.ClearProviders();
+//builder.Logging.AddConsole();
 // Add services to the container.
 
 builder.Services.AddControllers();
